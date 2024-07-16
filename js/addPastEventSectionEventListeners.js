6e3f4930f1f14aff771848b1939f4acd8fc0c15e
Fraser Miller <frasermiller0@gmail.com>
2024-07-16 15:00:07 +0100

    Fixed and Renamed script that adds event listener to past events section
    Fixed and renamed script that adds listener to past events section so that it works reliably. removed redundant script.

---
+++ js/addPastEventSectionEventListeners.js
@@ -0,0 +1,10 @@
+function addPastEventSectionEventListeners() {
+    let codeOlympicsTabOption = document.getElementById("code-olympics-tab-option");
+    let dyhtgTabOption = document.getElementById("dyhtg-tab-option");
+    let minorEventsTabOption = document.getElementById("minor-past-events-tab-option");
+    console.log("test")
+    codeOlympicsTabOption.addEventListener('click', function() {console.log("test")});
+    dyhtgTabOption.addEventListener('click', function() {console.log("test")});
+    minorEventsTabOption.addEventListener('click', function() {console.log("test")});
+}
+
