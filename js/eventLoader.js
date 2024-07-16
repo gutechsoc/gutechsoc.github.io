615ecb11c28b2f3801ec9bccf1fcec4cc67aa1c7
luke102 <lukeormiston@gmail.com>
2024-07-16 14:13:29 +0100

    Styling of event card

--- js/eventLoader.js
+++ js/eventLoader.js
@@ -1,7 +1,6 @@
-// Event type = [section on the DOM, how many events have been shown, the key in eventsList.json]
-let codeolympics = [null, 0, "codeolympics"];
-let dyhtguts = [null, 0, "dyhtguts"];
-let other = [null, 0, "other"];
+let codeolympics = {"targetElement": null, "numEventsAdded": 0, "id": "codeolympics"};
+let dyhtguts = {"targetElement": null, "numEventsAdded": 0, "id": "dyhtguts"};
+let other = {"targetElement": null, "numEventsAdded": 0, "id": "other"};
 
 let eventsList = null;
 
@@ -19,30 +18,30 @@ async function eventLoader() {
 
 // Add X amounts of events to a section
 async function addEvents(eventSection, amount) {
-    let startIndex = eventSection[1];
+    let startIndex = eventSection["numEventsAdded"];
 
     // Dont add more events if we've exhausted them all
-    if (startIndex === eventsList[eventSection[2]].length) {
-        console.log(`All events have been added for ${eventSection[2]}`);
+    if (startIndex === eventsList[eventSection["id"]].length) {
+        console.log(`All events have been added for ${eventSection["id"]}`);
         return;
     }
 
     // Dont try to add more events than we have left
-    if (startIndex + amount > eventsList[eventSection[2]].length) {
-        amount = eventsList[eventSection[2]].length - eventSection[1];
+    if (startIndex + amount > eventsList[eventSection["id"]].length) {
+        amount = eventsList[eventSection["id"]].length - eventSection["numEventsAdded"];
     }
 
     // Grab the old list of events to append to
     content = ``;
-    content += eventSection[0].innerHTML;
+    content += eventSection["targetElement"].innerHTML;
 
     // Append the requested number of events
     for (let i = startIndex ; i < startIndex + amount; i++) {
-        content += await getRequest(window.location.origin + '/events/' + eventsList[eventSection[2]][i]);
-        eventSection[1] += 1
+        content += await getRequest(window.location.origin + '/events/' + eventsList[eventSection["id"]][i]);
+        eventSection["numEventsAdded"] += 1
     }
 
-    eventSection[0].innerHTML = content;
+    eventSection["targetElement"].innerHTML = content;
 }
 
 // Get requester, returns the content of the request, handles errors
@@ -67,7 +66,15 @@ async function getRequest(url) {
 
 async function setup() {
     eventsList = await getRequest(window.location.origin + '/events/eventsList.json');
-    codeolympics[0] = document.getElementById('upcoming-events');
-    dyhtguts[0] = document.getElementById('minor-past-events');
-    other[0] = document.getElementById('major-past-events');
+    codeolympics["targetElement"] = document.getElementById('upcoming-events');
+    dyhtguts["targetElement"] = document.getElementById('minor-past-events');
+    other["targetElement"] = document.getElementById('major-past-events');
+}
+
+function toggleEventDetails(element) {
+    if (element.nextElementSibling.style.display !== "flex") {
+        element.nextElementSibling.style.display = "flex";
+    } else {
+        element.nextElementSibling.style.display = "none";
+    }
 }
