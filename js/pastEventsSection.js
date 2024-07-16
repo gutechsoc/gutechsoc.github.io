0692717b6af9bd85352c770110c4d251e0d89e56
Fraser Miller <frasermiller0@gmail.com>
2024-07-16 14:34:53 +0100

    [WIP]Added Past Events Section
    Added the styling for the past events section.
    
    Added a script to add event listeners to events section, non functional as of commit, thus WIP designation

---
+++ js/pastEventsSection.js
@@ -0,0 +1,7 @@
+codeOlympicsTabOption = document.getElementById("code-olympics-tab-option");
+dyhtgTabOption = document.getElementById("dyhtg-tab-option");
+minorEventsTabOption = document.getElementById("minors-events-option");
+
+codeOlympicsTabOption.addEventListener('click', function() {console.log("test")});
+dyhtgTabOption.addEventListener('click', function() {console.log("test")});
+minorEventsTabOption.addEventListener('click', function() {console.log("test")});
