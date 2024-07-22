e0dcd619308386d536190408c89dd5cea8369515
Fraser Miller <frasermiller0@gmail.com>
2024-07-22 16:50:19 +0100

    Removed redundant script file

--- js/pastEventsSection.js
+++
@@ -1,7 +0,0 @@
-codeOlympicsTabOption = document.getElementById("code-olympics-tab-option");
-dyhtgTabOption = document.getElementById("dyhtg-tab-option");
-minorEventsTabOption = document.getElementById("minors-events-option");
-
-codeOlympicsTabOption.addEventListener('click', function() {console.log("test")});
-dyhtgTabOption.addEventListener('click', function() {console.log("test")});
-minorEventsTabOption.addEventListener('click', function() {console.log("test")});
