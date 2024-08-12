5ea8b60d6af53ed7eefaf0d6f570868fc396f198
Fraser Miller <frasermiller0@gmail.com>
2024-08-12 12:18:43 +0100

    Added expandable property to general dropdown
    Added a expandable property to general dropdown which would increase the expanded width of the dropdown menu. Currently uses a set value that is shared across all general dropdown elements

--- js/genericDropdown.js
+++ js/genericDropdown.js
@@ -1,11 +1,25 @@
 function dropdown(toggleElement){
     let contentElement = toggleElement.nextElementSibling
+    let expandedElementLimit = 200
 
     if(toggleElement.classList.contains("selected")){
+        console.log("here")
         toggleElement.classList.remove("selected")
+        if(toggleElement.parentElement.parentElement.classList.contains("expandable")){
+            expand(toggleElement.parentElement, -expandedElementLimit)
+        }
         contentElement.style.maxHeight = 0 + "px";
     }else{
         toggleElement.classList.add("selected")
+        if(contentElement.parentElement.parentElement.classList.contains("expandable")){
+            expand(toggleElement.parentElement, expandedElementLimit)
+        }
         contentElement.style.maxHeight= contentElement.scrollHeight + "px"
+
     }
+
+    function expand(element, amount){
+        element.style.width = (element.clientWidth + amount) + "px"
+    }
+
 }
