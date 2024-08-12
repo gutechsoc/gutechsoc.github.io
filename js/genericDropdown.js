411f789ad4e110286f27e35679d98c2780ad3bbf
Fraser Miller <frasermiller0@gmail.com>
2024-08-12 16:03:59 +0100

    Added members section along with updates to generic dropdown function
    removed expandable attribute from generic dropdown class as it clashed with other methods altering the width of the generic dropdown elements

--- js/genericDropdown.js
+++ js/genericDropdown.js
@@ -1,24 +1,12 @@
 function dropdown(toggleElement){
     let contentElement = toggleElement.nextElementSibling
-    let expandedElementLimit = 200
-
     if(toggleElement.classList.contains("selected")){
         toggleElement.classList.remove("selected")
-        if(toggleElement.parentElement.parentElement.classList.contains("expandable")){
-            expand(toggleElement.parentElement, -expandedElementLimit)
-        }
         contentElement.style.maxHeight = 0 + "px";
     }else{
         toggleElement.classList.add("selected")
-        if(contentElement.parentElement.parentElement.classList.contains("expandable")){
-            expand(toggleElement.parentElement, expandedElementLimit)
-        }
         contentElement.style.maxHeight= contentElement.scrollHeight + "px"
-
     }
 
-    function expand(element, amount){
-        element.style.width = (element.clientWidth + amount) + "px"
-    }
 
 }
