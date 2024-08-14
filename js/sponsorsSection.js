7d491f6285c6e288fe512c1bd1c75f0ad374762c
Fraser Miller <frasermiller0@gmail.com>
2024-08-14 14:58:05 +0100

    Added list of desired sponsors for sponsors marquee and added gradient to sponsors marquee

---
+++ js/sponsorsSection.js
@@ -0,0 +1,8 @@
+/*  In this function, write the name of EXACTLY 12 sponsors that you would like to show in the sponsors marquees in the sponsors section
+*   The names should be written as they appear as KEYS in sponsors.json (NOT THE "name" ATTRIBUTE)
+*   If you would like more than 12 sponsors then contact me via discord and I'll make the changes
+*   (just because you have to mess around with a bunch of style components and it's a faff)
+ */
+function getMarqueeSponsors(){
+    return ["amzn","barclays","broadridge","compsoc","craneware","cyberpro","dogfish","dsg","ef","github","dominos","cooper_software"]
+}
