709481321ad54be2abb1b39a03f2622436a531c8
Fraser Miller <frasermiller0@gmail.com>
2024-08-20 14:18:55 +0100

    Fixed members pannel expanding past max height of boundary
    Added a 450 px additional max-height to the element housing the members mugshots to account for members being hidden due to expansion of member's info pushing elements past max height of display element (boundary)

--- js/membersSection.js
+++ js/membersSection.js
@@ -1,4 +1,6 @@
 function membersDropdown(element){
+    // addition to max height in px
+    let heightExpansionBuffer = 450
     // Gets list of currently selected members and deselects them before calling dropdown
     let selectedMembers = document.querySelectorAll(".member.selected .mugshot")
     if(selectedMembers.length > 0){
@@ -7,13 +9,16 @@ function membersDropdown(element){
         })
     }
     dropdown(element)
+    //adds an extra px of length to max height to account for members info expanding
+    if(element.classList.contains("selected")){
+        element.nextElementSibling.style.maxHeight = element.nextElementSibling.scrollHeight + heightExpansionBuffer +"px"
+    }
 
 }
 
 function expandMember(element){
     // Expands and retracts board member info
     let member = element.parentElement
-    let memberInfo = element.nextElementSibling
     if(member.classList.contains("selected")){
         member.classList.remove("selected")
     }else{
