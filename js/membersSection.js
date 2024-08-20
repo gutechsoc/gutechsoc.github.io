5f66a92e6e0f20a06bebe3bc194ddf5f736a5c6a
Fraser Miller <frasermiller0@gmail.com>
2024-08-20 18:10:35 +0100

    Only one member-info visible at a time
    Added delay to members info appearing when another members info section is visible to avoid unintented behaviour with member alignment (members jumping to lower line when adjacent member is being expanted)

--- js/membersSection.js
+++ js/membersSection.js
@@ -1,28 +1,38 @@
+let someMemberOpen = false
+
 function membersDropdown(element){
-    // addition to max height in px
-    let heightExpansionBuffer = 450
     // Gets list of currently selected members and deselects them before calling dropdown
+    collapseAllMembers()
+    dropdown(element);
+}
+
+function expandMember(element) {
+    // Expands and retracts board member info
+    // One board member expanded at a time
+    // Adds delay to allow other elements to collapse to avoid card jumping between lines
+    let timeDelay = 0;
+    let member = element.parentElement
+
+    if (someMemberOpen && !member.classList.contains("selected")) {
+        timeDelay = 300
+        collapseAllMembers()
+    }
+    setTimeout(function(){
+        if (member.classList.contains("selected")) {
+            member.classList.remove("selected")
+        } else {
+            member.classList.add("selected")
+            someMemberOpen = true
+    }},timeDelay)
+}
+
+
+function collapseAllMembers(){
     let selectedMembers = document.querySelectorAll(".member.selected .mugshot")
     if(selectedMembers.length > 0){
         Array.from(selectedMembers).forEach(member => {
-            expandMember(member)
+            member.parentElement.classList.remove("selected");
         })
     }
-    dropdown(element)
-    //adds an extra px of length to max height to account for members info expanding
-    if(element.classList.contains("selected")){
-        element.nextElementSibling.style.maxHeight = element.nextElementSibling.scrollHeight + heightExpansionBuffer +"px"
-    }
-
-}
-
-function expandMember(element){
-    // Expands and retracts board member info
-    let member = element.parentElement
-    if(member.classList.contains("selected")){
-        member.classList.remove("selected")
-    }else{
-        member.classList.add("selected")
-        refreshEventGroupHeight(member)
-    }
+    someMemberOpen=false
 }
