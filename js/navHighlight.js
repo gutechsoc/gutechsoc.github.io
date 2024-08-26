60546698580457103b4fca02e7913b714de9b584
Fraser Miller <frasermiller0@gmail.com>
2024-08-26 17:10:36 +0100

    Navbar highlight threasholds now dynamic depending on page location
    Navbar highlight threashold used to be static region in the middle 50% of the viewport. This means it could not physically highlight the contact us section.
    Navbar highlight now sets the threashold to the top/bottom 50% of the page depending on if you where at the top/bottom scroll limit of the page respectivly.

--- js/navHighlight.js
+++ js/navHighlight.js
@@ -14,11 +14,24 @@ function scrollSetup() {
 
     addEventListener("scroll", function() {
 
-        // If the section is within 50% of the window centered in the middle, highlight the navigation element
-        windowHeight = window.visualViewport.height
-        console.log(windowHeight);
-        topCutOff = (window.visualViewport.height / 2) - (window.visualViewport.height / 4)
-        bottomCutOff = (window.visualViewport.height / 2) + (window.visualViewport.height / 4)
+        //Cutoff is top 50% if at the top of the page, middle 50% if in the middle and bottom 50% if at the bottom of the page
+        let documentHeightExtreme = 20
+        let windowHeight = window.visualViewport.height
+        let documentHeight = document.body.scrollHeight
+        let topCutOff = 0;
+        let bottomCutOff = 0
+
+        if(window.scrollY < documentHeightExtreme){
+            topCutOff = 0
+            bottomCutOff = windowHeight/2
+        }else if((windowHeight + window.scrollY) - documentHeight < documentHeightExtreme && (windowHeight + window.scrollY) - documentHeight > 0){
+            topCutOff = windowHeight/2
+            bottomCutOff = documentHeight
+        }else{
+            topCutOff = (window.visualViewport.height / 2) - (window.visualViewport.height / 4)
+            bottomCutOff = (window.visualViewport.height / 2) + (window.visualViewport.height / 4)
+        }
+
 
         sections.forEach(section => {
             boundingBox = section[0].getBoundingClientRect()
